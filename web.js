bde65e3f2b2a92bd1fe2f3f10895a61265983f4c
zglicz <mzglicz@gmail.com>
2013-07-04 18:45:14 +0200

    Add whitespace

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 var fs = require('fs')
 
+
 app.get('/', function(request, response) {
   var index_contents = fs.readFileSync('index.html');
   var wartosci = new Buffer(index_contents);
