e01dfa8c93ba28a86498bac92b97094e7d130007
zglicz <mzglicz@gmail.com>
2013-07-04 18:37:44 +0200

    Update web.js to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs')
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+
+  var index_contents = fs.readFileSync('index.html');
+  var wartosci = new Buffer(index_contents);
+  response.send(wartosci.toString());
 });
 
 var port = process.env.PORT || 5000;
